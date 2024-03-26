1efe13a23f90609260248013ac93f40d09eb6dda
Julie Lycklama <julyckla@microsoft.com>
2024-03-25 17:38:12 -0700

    display timeout behavior

---
+++ HandleTask.cs
@@ -0,0 +1,24 @@
+﻿namespace PollySample
+{
+    internal class HandleTask
+    {
+        private readonly TimeSpan _duration;
+
+        public HandleTask(TimeSpan duration)
+        {
+            _duration = duration;
+        }
+
+        public async Task<bool> ExecuteAsync(CancellationToken cancellationToken)
+        {
+            await Task.Delay(_duration, cancellationToken);
+            return true;
+        }
+
+        public bool Execute()
+        {
+            Task.Delay(_duration).Wait();
+            return true;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,56 @@
+﻿using Polly.Timeout;
+using Polly;
+using PollySample;
+
+var pipeline = new ResiliencePipelineBuilder<bool>()
+            .AddTimeout(new TimeoutStrategyOptions
+            {
+                Timeout = TimeSpan.FromSeconds(1),
+            })
+            .AddRetry(new()
+            {
+                Delay = TimeSpan.FromMilliseconds(1),
+                MaxRetryAttempts = int.MaxValue,
+                BackoffType = DelayBackoffType.Constant,
+                ShouldHandle = args => ValueTask.FromResult(args.Outcome.Result),
+            })
+            .Build();
+
+var longTask = new HandleTask(TimeSpan.FromSeconds(2));
+CancellationToken cancellationToken = new CancellationToken();
+
+// cancellation token used to cancel the task, TimeoutRejectedException is thrown
+try
+{
+    await pipeline.ExecuteAsync<bool>(ct => new ValueTask<bool>(longTask.ExecuteAsync(ct)), cancellationToken);
+
+    Console.WriteLine("Attempt 1: Task completed successfully");
+}
+catch (TimeoutRejectedException e)
+{
+    Console.WriteLine("Attempt 1 failed", e);
+}
+
+// cancellation token is not used to cancel the task, no exception is thrown, pipeline finishes even though our condition has not been met
+try
+{
+    await pipeline.ExecuteAsync<bool>(ct => new ValueTask<bool>(longTask.Execute()), cancellationToken);
+
+    Console.WriteLine("Attempt 2: Task completed successfully");
+}
+catch (TimeoutRejectedException e)
+{
+    Console.WriteLine("Attempt 2 failed", e);
+}
+
+// cancellation token is not used to cancel the task, no exception is thrown, pipeline finishes even though our condition has not been met
+try
+{
+    pipeline.Execute<bool>(ct => longTask.Execute(), cancellationToken);
+
+    Console.WriteLine("Attempt 3: Task completed successfully");
+}
+catch (TimeoutRejectedException e)
+{
+    Console.WriteLine("Attempt 3 failed", e);
+}
